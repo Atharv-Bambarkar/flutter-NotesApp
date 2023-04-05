09d12e8dcdbf01a6f20b278920ec7cc9d362cbca
Atharv-Bambarkar <atharvbambarkar@gmail.com>
2023-04-05 10:43:30 +0530

    First Commit

---
+++ NotesApp/lib/main.dart
@@ -0,0 +1,215 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Firestore',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+// text fields' controllers
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _subtitlesController = TextEditingController();
+
+  final CollectionReference _notes =
+  FirebaseFirestore.instance.collection('notes');
+
+  Future<void> _create([DocumentSnapshot? documentSnapshot]) async {
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(labelText: 'title'),
+                ),
+                TextField(
+                  /*keyboardType:
+                  const TextInputType.numberWithOptions(decimal: false),*/
+                  controller: _subtitlesController,
+                  decoration: const InputDecoration(
+                    labelText: 'Subtitles',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text('Create'),
+                  style: ElevatedButton.styleFrom(primary: Colors.greenAccent),
+                  onPressed: () async {
+                    final String title = _titleController.text;
+                    final String subtitles = _subtitlesController.text;
+                    if (subtitles != null) {
+                      await _notes.add({"title": title, "subtitles": subtitles });
+
+                      _titleController.text = '';
+                      _subtitlesController.text = '';
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+
+        });
+  }
+  Future<void> _update([DocumentSnapshot? documentSnapshot]) async {
+    if (documentSnapshot != null) {
+
+      _titleController.text = documentSnapshot['title'];
+      _subtitlesController.text = documentSnapshot['subtitles'];
+    }
+
+    await showModalBottomSheet(
+        isScrollControlled: true,
+        context: context,
+        builder: (BuildContext ctx) {
+          return Padding(
+            padding: EdgeInsets.only(
+                top: 20,
+                left: 20,
+                right: 20,
+                bottom: MediaQuery.of(ctx).viewInsets.bottom + 20),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _titleController,
+                  decoration: const InputDecoration(labelText: 'title'),
+                ),
+                TextField(
+                  /*keyboardType:
+                  const TextInputType.numberWithOptions(decimal: true),*/
+                  controller: _subtitlesController,
+                  decoration: const InputDecoration(
+                    labelText: 'Subtitles',
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  child: const Text( 'Update'),
+                  style: ElevatedButton.styleFrom(primary: Colors.greenAccent),
+                  onPressed: () async {
+                    final String title = _titleController.text;
+                    final  String subtitles = _subtitlesController.text;
+                    if (subtitles != null) {
+
+                      await _notes
+                          .doc(documentSnapshot!.id)
+                          .update({"title": title, "subtitles": subtitles});
+                      _titleController.text = '';
+                      _subtitlesController.text = '';
+                      Navigator.of(context).pop();
+                    }
+                  },
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  Future<void> _delete(String productId) async {
+    await _notes.doc(productId).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text('You have successfully deleted a Notes')));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(child: Text('MY NOTES')),
+          backgroundColor: Colors.greenAccent,
+        ),
+        body: StreamBuilder(
+          stream: _notes.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                  streamSnapshot.data!.docs[index];
+                  return Card(
+                    margin: const EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text(documentSnapshot['title']),
+                      subtitle: Text(documentSnapshot['subtitles']),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                icon: const Icon(Icons.edit),
+                                onPressed: () =>
+                                    _update(documentSnapshot)),
+                            IconButton(
+                                icon: const Icon(Icons.delete),
+                                onPressed: () =>
+                                    _delete(documentSnapshot.id)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          },
+        ),
+// Add new product
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => _create(),
+          backgroundColor: Colors.greenAccent,
+          child: const Icon(Icons.add)
+          ,
+
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat
+    );
+  }
+}
